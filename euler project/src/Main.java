ccd37facb102f317c2ce2b770ba11eb16eb98dce
pourya1382 <115644088+pourya1382@users.noreply.github.com>
2022-10-13 09:58:46 +0330

    Update Main.java

--- euler project/src/Main.java
+++ euler project/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
     Scanner answer = new Scanner(System.in);
     int n = answer.nextInt();
-    for(int i = 0; i<=n ;i++){
+    for(int i = 0; i<=n ;i+2){
 
 
     }
