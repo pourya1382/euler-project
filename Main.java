91e7a5d0529b1ba5d4e7ee50bedc5b1a44918d9c
pourya1382 <115644088+pourya1382@users.noreply.github.com>
2022-10-13 10:46:58 +0330

    test

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner answer = new Scanner(System.in);
+    int n = answer.nextInt();
+    for(int i = 0; i<=n ;i++){
+
+
+    }
+}}
