ca755f19e88e0c8b4d4759c10223f6c0292402b1
Hpkarimi <hpkarimi110@gmail.com>
2022-10-16 18:07:05 +0330

    first problem

--- euler project/src/Main.java
+++ euler project/src/Main.java
@@ -1,11 +1,48 @@
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
-    Scanner answer = new Scanner(System.in);
-    int n = answer.nextInt();
-    for(int i = 0; i<=n ;i+2){
+        Scanner scanner = new Scanner(System.in);
+        int primeNumber = scanner.nextInt();
+        int i = 1;
+        int [] listPrimeNumber = new int[primeNumber];
+
+        while(i == primeNumber){
 
 
+            int x = 5;
+
+            int [] mainPrime = new int[i];
+            for(int y =0;y<i;y++){
+                if(listPrimeNumber[y]!=0){
+                    int z = 1;
+                    mainPrime[z] = listPrimeNumber[y];
+                    z++;
+                }
+
+            }
+            if(x%2!=0){
+                listPrimeNumber[i] =x ;
+                x+=2;
+            } else if (x% mainPrime[0]!=0) {
+                listPrimeNumber[i] =x ;
+                x+=2;
+            }
+            {
+
+            }
+
+
+
+
+
+
+        }
+        System.out.println("your number: " + listPrimeNumber[primeNumber] );
     }
-}}
+
+}
+
+
---
+++ euler project/src/euler.java
@@ -0,0 +1,2 @@
+public class euler {
+}
