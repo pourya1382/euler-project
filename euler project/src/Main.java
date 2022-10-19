16c29d86a4ce35f32d3823dd6add0629ef7e5e62
Hpkarimi <hpkarimi110@gmail.com>
2022-10-19 12:44:46 +0330

    is that a prime number?!

--- euler project/src/Main.java
+++ euler project/src/Main.java
@@ -5,33 +5,49 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println("enter prime number index :");
         Scanner scanner = new Scanner(System.in);
         int primeNumber = scanner.nextInt();
-        ArrayList <Integer> primeNumbers= new ArrayList<Integer>();
-        int prime=3;
-
-
-        while (primeNumbers.size() == primeNumber){
-            for(int i=0 ; i == primeNumbers.size(); i++) {
-                int result = prime / primeNumbers.get(i);
-                if(result == 0){
-                    primeNumbers.remove(result);
-                    continue;
-                } else{
-                    primeNumbers.add(result);
+        ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+        primeNumbers.add(2);
+        int prime = 3;
+        int halfPrimeNumber = primeNumber / 2;
+        for (int i = 0; i < halfPrimeNumber + primeNumber; i++) {
+            int result = primeNumber % halfPrimeNumber;
+            if (result == 0) {
+                System.out.println("this isn,t prime number");
+                break;
+            } else {
+                halfPrimeNumber -= 1;
+                if (halfPrimeNumber == 1) {
+                    System.out.println("this  is prime number");
+                    break;
                 }
+            }
 
 
-
-                }
-                prime +=2;
-                }
+        }
 
 
-        System.out.println(prime);
+//        while (primeNumbers.size() != primeNumber) {
+//            System.out.println("primeNumbers.size(): " + primeNumbers.size());
+//            for (int i = 0; i < primeNumbers.size(); i++) {
+//                System.out.println("i:" + i);
+//                int result = prime % primeNumbers.get(i);
+//                if (result == 0) {
+//                    break;
+//                } else {
+//
+//                }
+//                System.out.println(primeNumbers);
+//            }
+//            prime += 2;
+//        }
+
+
+//        System.out.println(prime);
 
     }
-
 
 
 }
