9acd36256690e8c47214c5641dfd2bd036b8dde7
Hpkarimi <hpkarimi110@gmail.com>
2022-10-19 13:17:02 +0330

    for loop is correct

--- euler project/src/Main.java
+++ euler project/src/Main.java
@@ -11,46 +11,60 @@ public class Main {
         ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
         primeNumbers.add(2);
         int prime = 3;
-        int halfPrimeNumber = primeNumber / 2;
-        for (int i = 0; i < halfPrimeNumber + primeNumber; i++) {
-            int result = primeNumber % halfPrimeNumber;
-            if (result == 0) {
-                System.out.println("this isn,t prime number");
-                break;
-            } else {
-                halfPrimeNumber -= 1;
-                if (halfPrimeNumber == 1) {
-                    System.out.println("this  is prime number");
+//        int halfPrimeNumber = primeNumber / 2;
+//        for (int i = 0; i < halfPrimeNumber + primeNumber; i++) {
+//            int result = primeNumber % halfPrimeNumber;
+//            if (result == 0) {
+//                System.out.println("this isn,t prime number");
+//                break;
+//            } else {
+//                halfPrimeNumber -= 1;
+//                if (halfPrimeNumber == 1) {
+//                    System.out.println("this  is prime number");
+//                    break;
+//                }
+//            }
+//
+//
+//        }
+//
+//
+        while (primeNumbers.size() != primeNumber) {
+            System.out.println("primeNumbers.size(): " + primeNumbers.size());
+            for (int i = 0; i < primeNumbers.size(); i++) {
+                System.out.println("i:" + i);
+                int result = prime % primeNumbers.get(i);
+                System.out.println("result :"+ result);
+                if (result == 0) {
+
+                    if (primeNumbers.contains(prime)) {
+                        int index = primeNumbers.indexOf(prime);
+                        primeNumbers.remove(index);
+                        break;
+
+                    }
                     break;
                 }
+
+
+
+
             }
-
-
+            primeNumbers.add(prime);
+            System.out.println("prime:" + prime);
+            prime += 2;
+            System.out.println(primeNumbers);
         }
-
-
-//        while (primeNumbers.size() != primeNumber) {
-//            System.out.println("primeNumbers.size(): " + primeNumbers.size());
-//            for (int i = 0; i < primeNumbers.size(); i++) {
-//                System.out.println("i:" + i);
-//                int result = prime % primeNumbers.get(i);
-//                if (result == 0) {
-//                    break;
-//                } else {
-//
-//                }
-//                System.out.println(primeNumbers);
-//            }
-//            prime += 2;
-//        }
-
-
-//        System.out.println(prime);
-
     }
-
-
 }
+
+
+
+
+
+
+
+
 
 
 
